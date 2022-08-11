bef62d3eece3094ef54a04eba0bd3ba94d8f5e5d
duermomal <110434883+duermomal@users.noreply.github.com>
2022-08-10 22:06:45 -0300

    validacion de campos vacios

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,16 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+const validar = document.getElementById("regBtn");
+
+validar.addEventListener("click", () => {    
+   let campos_form = document.getElementsByClassName("form-control");
+   for(let campo of campos_form){
+       if(campo.value.length === 0){//campos vacios
+           showAlertError();
+       }
+   }
+
+});
+
